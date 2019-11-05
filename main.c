2b7ba1ae8749a3c556ecf3ae67e16db3d57be978
boltyu <lightingyu@gmail.com>
2019-11-05 18:27:54 +0800

    20191105

--- main.c
+++ main.c
@@ -4,22 +4,27 @@
  */
 #include "main.h"
 
-void waitfor(void){
-	int countx = 1000;
-	while(countx--);
-		
-}
+
  
 void main(void)
 {
-	PC_DDR = 0xff;
-	PC_CR1 = 0;
-	PC_CR2 = 0;
+	unsigned int delaycount = 0;
 	
-	for(;;){
-		PC_ODR = 0xff;
-		waitfor();
-		PC_ODR = 0x00;
-		waitfor();		
-		}
+	CONFIG_UNUSED_PINS_STM8L001;
+	
+	STARTUP_SWIM_DELAY_5S;
+	
+	GPIO_Init(GPIOA, GPIO_Pin_0, GPIO_Mode_In_PU_No_IT);
+	GPIO_Init(GPIOA, GPIO_Pin_2, GPIO_Mode_In_PU_No_IT);
+	GPIO_Init(GPIOD, GPIO_Pin_0, GPIO_Mode_In_PU_No_IT);
+	GPIO_Init(GPIOB, GPIO_Pin_6, GPIO_Mode_In_PU_No_IT);
+	GPIO_Init(GPIOB, GPIO_Pin_7, GPIO_Mode_In_PU_No_IT);
+	
+	GPIO_Init(GPIOB, GPIO_Pin_3 ,GPIO_Mode_Out_PP_Low_Slow);
+	
+	while(1){
+		delaycount = 10000;
+		while(delaycount --);
+		GPIO_ToggleBits(GPIOB,GPIO_Pin_3);
+	}
 }
--- main.h
+++ main.h
@@ -1,7 +1,40 @@
 #ifndef __MAIN_H__
 #define __MAIN_H__
 
-#include <iostm8l001.h>
+
+
+
+
+#define _COSMIC_
+
+#include <stm8l10x.h>
+
+#define STARTUP_SWIM_DELAY_5S \
+{\
+_asm(\
+"				PUSHW X				\n"	\
+" 			PUSH A				\n"	\
+"				LDW X,#0xFFFF	\n"	\
+"loop1:	LD 	A,#50			\n"	\
+													\
+"loop2: DEC A					\n"	\
+"				JRNE loop2		\n"	\
+													\
+"				DECW X				\n"	\
+"				JRNE loop1		\n"	\
+													\
+"				POP A					\n"	\
+"				POPW X					");\
+}
+
+
+#define CONFIG_UNUSED_PINS_STM8L001 \
+{\
+	GPIOA->DDR |= GPIO_Pin_1 | GPIO_Pin_3 | GPIO_Pin_5;\
+	GPIOB->DDR |= GPIO_Pin_0 | GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_4;\
+	GPIOC->DDR |= GPIO_Pin_5 | GPIO_Pin_6;\
+	GPIOD->DDR |= GPIO_Pin_1 | GPIO_Pin_2 | GPIO_Pin_3 | GPIO_Pin_4 | GPIO_Pin_5 | GPIO_Pin_6 | GPIO_Pin_7;\
+}
 
 
 #endif
